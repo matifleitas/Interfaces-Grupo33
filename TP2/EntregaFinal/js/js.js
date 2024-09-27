9e20533ae81e672e67c22593cc555a5196c51876
Isaias Leonel Sardina <isaias.sardina@gmail.com>
2024-09-27 12:51:05 -0300

    Carrusel Primary en estado: 75%

--- TP2/EntregaFinal/js/js.js
+++ TP2/EntregaFinal/js/js.js
@@ -2,7 +2,6 @@ let card = document.querySelectorAll(".card-game");
 let btnNext = document.querySelector(".arrowRigth").addEventListener(
     "click", (e) => {
         for (let index = 0; index < card.length; index++) {
-            console.log("Entre")
             card[index].style.transform = "translateX(-40%)"
             card[index].style.transition = "2s"
         }
@@ -11,7 +10,6 @@ let btnNext = document.querySelector(".arrowRigth").addEventListener(
 let btnBefore = document.querySelector(".arrowLeft").addEventListener(
     "click", (e) => {
         for (let index = 0; index < card.length; index++) {
-            console.log("Entre")
             card[index].style.transform = "translateX(0)"
             card[index].style.transition = "2s"
         }
