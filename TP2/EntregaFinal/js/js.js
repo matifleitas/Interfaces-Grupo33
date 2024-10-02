f1de46170d11034dd60d171383eabd7888c5f7e7
Isaias Leonel Sardina <isaias.sardina@gmail.com>
2024-10-02 19:43:17 -0300

    Menu Hambur

--- TP2/EntregaFinal/js/js.js
+++ TP2/EntregaFinal/js/js.js
@@ -1,21 +1,68 @@
+'use strict'
 let cards = document.querySelectorAll(".cardGame");
-let cardSecundary = document.querySelectorAll(".cardGameSecundary");
+// let cardSecundary = document.querySelectorAll(".cardGameSecundary");
 
-let btnNext = document.querySelector(".arrowRigth").addEventListener(
-    "click", (e) => {
-        translateCarrousel(cards);
-    });
+// let btnNext = document.querySelector(".arrowRigth").addEventListener(
+//     "click", (e) => {
+//         translateCarrousel(cards);
+//     });
 
-let btnNextSec = document.querySelector("#btnNext-carruselSec").addEventListener(
-    "click", (e) => {
-        translateCarrousel(cardSecundary);
-    });
+// let btnNextSec = document.querySelector("#btnNext-carruselSec").addEventListener(
+//     "click", (e) => {
+//         translateCarrousel(cardSecundary);
+//     });
 
-let btnBefore = document.querySelector(".arrowLeft").addEventListener(
-    "click", (e) => {
-        translateCarrouselLeft(cards);
-    }
-)
+// let btnBefore = document.querySelector(".arrowLeft").addEventListener(
+//     "click", (e) => {
+//         translateCarrouselLeft(cards);
+//     }
+// )
+
+// let btnBeforeSecundary = document.querySelector("#btnBefore-carruselSec").addEventListener(
+//     "click", (e) => {
+//         translateCarrouselLeft(cardSecundary)
+//     }
+// )
+
+let btnNext = document.querySelectorAll(".arrowRigth").addEventListener();
+
+btnNext.array.forEach(btn => {
+    btn.addEventListener(
+        "click", () => {
+            translateCarrouselLeft(cards)
+        }
+    )
+});
+
+let btnBefore = document.querySelectorAll(".arrowLeft").addEventListener();
+
+btnBefore.array.forEach(btn => {
+    btn.addEventListener(
+        "click", () => {
+            console.log("leegie")
+            translateCarrouselLeft(cards)
+        }
+    )
+});
+
+// let conteinerVertical = document.querySelector(".vertical-nav-wrapper")
+let verticalNav = document.querySelector(".vertical-nav-wrapper")
+document.querySelector('.btn-hamburguesa').addEventListener(
+    'click', () => {
+        if (verticalNav.style.transform === "translate(0%)") {
+            verticalNav.style.transform = "translate(-100%)";
+            conteinerVertical.style.transform = "translate(-100%)";
+        } else {
+            verticalNav.style.transform = "translate(0%)";
+        }
+        // document.querySelector('.vertical-nav-wrapper').classList.toggle('active');
+    })
+
+
+
+
+
+
 
 
 /*
@@ -27,26 +74,16 @@ login.addEventListener('submit', (event) => {
 })
 */
 
-
-  
-btnSubmit.addEventListener('click', ocultarFormulario);
-let btnBeforeSecundary = document.querySelector("#btnBefore-carruselSec").addEventListener(
-    "click", (e) => {
-        translateCarrouselLeft(cardSecundary)
-    }
-)
-
-
 // -----------funciones-------------
 function translateCarrousel(cardSize) {
     for (let index = 0; index < cardSize.length; index++) {
         cardSize[index].style.transform = "translateX(-40%)"
-        cardSize[index].style.transition = "2s"
+        cardSize[index].style.transition = "0.5s"
     }
 }
 function translateCarrouselLeft(cardSize) {
     for (let index = 0; index < cardSize.length; index++) {
         cardSize[index].style.transform = "translateX(0)"
-        cardSize[index].style.transition = "2s"
+        cardSize[index].style.transition = "0.5s"
     }
 }
