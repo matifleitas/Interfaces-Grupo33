66c5769cd3ad6f4e4864514e526e03dc3227cfe3
Matias Fleitas <matifleitas6@gmail.com>
2024-10-06 23:40:22 -0300

    preloader

--- TP2/EntregaFinal/preloader.js
+++ TP2/EntregaFinal/preloader.js
@@ -2,5 +2,5 @@ window.addEventListener("load", function() {
     setTimeout(function() {
         document.getElementById("preloader").style.display = "none";
         document.body.classList.remove("hidden");
-    }, 5000); //  = 5 segundos
+    }, 4000); //  preguntar = 5 segundos
 });
