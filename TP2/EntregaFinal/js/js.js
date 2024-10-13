54752b874a9fd8dd7af1f2a380f793268ea77606
Isaias Leonel Sardina <isaias.sardina@gmail.com>
2024-10-13 13:16:22 -0300

    Mobile First -> avance : 80%

--- TP2/EntregaFinal/js/js.js
+++ TP2/EntregaFinal/js/js.js
@@ -1,42 +1,104 @@
 'use strict'
 
+let cards = document.querySelectorAll(".card-game")
 let btnNext = document.querySelectorAll(".arrowRigth");
 let btnBefore = document.querySelectorAll(".arrowLeft");
+let operacion = 0;
+let counter = 0
+let pixelWidth = 1;
+let widthImg = (1600 / cards.length) + pixelWidth;
+
 
 btnNext.forEach(btn => {
     btn.addEventListener("click", function (e) {
         // Encuentra el contenedor más cercano del carrusel en el que está el botón
         let cardContainer = e.target.closest(".conteiner-carruselPrimary");
-        // Selecciona solo las tarjetas dentro de ese contenedor
-        let cards = cardContainer.querySelectorAll(".card-game");
-        translateCarrousel(cards); // Mueve solo las tarjetas de ese carrusel
+        if (counter >= cards.length - 1) {
+            counter = 0;
+            operacion = 0;
+            // Selecciona solo las tarjetas dentro de ese contenedor
+            cards = cardContainer.querySelectorAll(".card-game");
+            translateCarrousel(cards, operacion);
+        }
+        else {
+            counter++;
+            operacion = operacion + widthImg;
+            cards = cardContainer.querySelectorAll(".card-game");
+            translateCarrousel(cards, operacion);
+        }
     });
 });
 
 btnBefore.forEach(btn => {
     btn.addEventListener("click", function (e) {
-        // Encuentra el contenedor más cercano del carrusel en el que está el botón
         let cardContainer = e.target.closest(".conteiner-carruselPrimary");
-        // Selecciona solo las tarjetas dentro de ese contenedor
-        let cards = cardContainer.querySelectorAll(".card-game");
-        translateCarrouselLeft(cards); // Mueve solo las tarjetas de ese carrusel
+        counter--;
+
+        if (counter < 0) {
+            counter = cards.length - 1;
+            cards = cardContainer.querySelectorAll(".card-game");
+            operacion = widthImg * (cards.length - 1);
+            translateCarrouselLeft(cards, operacion);
+        }
+        else {
+            operacion = operacion - widthImg;
+            cards = cardContainer.querySelectorAll(".card-game");
+            translateCarrouselLeft(cards, operacion);
+        }
     });
 });
 
+// -------------- botones para el Destkop------------------
+// btnNext.forEach(btn => {
+//     btn.addEventListener("click", function (e) {
+//         // Encuentra el contenedor más cercano del carrusel en el que está el botón
+//         let cardContainer = e.target.closest(".conteiner-carruselPrimary");
+//         cards = cardContainer.querySelectorAll(".card-game");
+//         translateCarrousel(cards);
+//     });
+// });
+
+// btnBefore.forEach(btn => {
+//     btn.addEventListener("click", function (e) {
+//         let cardContainer = e.target.closest(".conteiner-carruselPrimary");
+//         cards = cardContainer.querySelectorAll(".card-game");
+//         translateCarrouselLeft(cards); // Mueve solo las tarjetas de ese carrusel
+
+//     });
+// });
+
 // -----------funciones-------------
-function translateCarrousel(cardSize) {
+function translateCarrousel(cardSize, operacion) {
     for (let index = 0; index < cardSize.length; index++) {
-        cardSize[index].style.transform = "translateX(-40%)";
-        cardSize[index].style.transition = "0.5s";
+        cardSize[index].style.transform = `translate(-${operacion}%)`;
+        cardSize[index].style.transition = " ease-in-out .6s";
+
     }
 }
 
-function translateCarrouselLeft(cardSize) {
+function translateCarrouselLeft(cardSize, operacion) {
     for (let index = 0; index < cardSize.length; index++) {
-        cardSize[index].style.transform = "translateX(0)";
-        cardSize[index].style.transition = "0.5s";
+        cardSize[index].style.transform = `translate(-${operacion}%)`;
+        cardSize[index].style.transition = " ease-in-out .6s";
     }
 }
+
+
+// -----------------funciones para desktop----------
+// function translateCarrousel(cardSize) {
+//     for (let index = 0; index < cardSize.length; index++) {
+//         console.log(cardSize.length)
+//         cardSize[index].style.transform = "translateX(-25%)";
+//         cardSize[index].style.transition = "0.5s";
+//     }
+// }
+
+// function translateCarrouselLeft(cardSize) {
+//     for (let index = 0; index < cardSize.length; index++) {
+//         cardSize[index].style.transform = "translateX(0)";
+//         cardSize[index].style.transition = "0.5s";
+//     }
+// }
 
 let verticalNav = document.querySelector(".vertical-nav-wrapper")
 document.querySelector('.btn-hamburguesa').addEventListener(
