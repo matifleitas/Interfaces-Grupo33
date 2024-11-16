7f849166f027b0790e3d2ddd064b3c97209f60ef
Matias Fleitas <matifleitas6@gmail.com>
2024-11-16 20:46:13 -0300

    mouseMove parallax

--- TP4/EntregaFinal/js/main.js
+++ TP4/EntregaFinal/js/main.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
         logo.style.transform = "scale(0.4)";  
         logo.style.transition = "transform 0.6s ease";
         logo.style.position = "absolute";
-        logo.style.top = "90px";
+        logo.style.top = "10px";
         logo.style.marginLeft = "10px";
         logo.style.zIndex = "3";
 
@@ -44,3 +44,22 @@ document.addEventListener("DOMContentLoaded", function () {
         }); */
     });
 });
+
+(function () {
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#parallaxPjs");
+    function parallax(e) {
+        const _w = window.innerWidth / 2; //mitad de pantalla
+        const _h = window.innerHeight / 2;
+        const _mouseX = e.clientX;
+        const _mouseY = e.clientY;
+
+        const _depth1 = `${75.6 - (_mouseX - _w) * 0.002}% ${75.6 - (_mouseY - _h) * 0.002}%`; // movimiento en direccion contrario al mouse
+        const _depth2 = `${75.6 - (_mouseX - _w) * 0.004}% ${75.6 - (_mouseY - _h) * 0.004}%`;
+        const _depth3 = `${75.6 - (_mouseX - _w) * 0.008}% ${75.6 - (_mouseY - _h) * 0.008}%`;
+
+        const x = `${_depth3}, ${_depth2}, ${_depth1}`; 
+        elem.style.backgroundPosition = x; //lo mueve
+    }
+})();
+
